c140206c3f3f68a872c742bc6db815388d9747a8
Mark Tomlin <Dygear@gmail.com>
2015-07-03 23:07:26 -0400

    Update install.sh
    Changed NGINX's prefix to the default, and apparently they don't offer https on the download server.

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ apt-get install build-essential libgd2-xpm-dev libgeoip-dev libpcre3-dev libssl-
 
 cd ~
 
-wget https://nginx.org/download/nginx-1.9.2.tar.gz
+wget http://nginx.org/download/nginx-1.9.2.tar.gz
 tar xvfz nginx-1.9.2.tar.gz
 cd nginx-1.9.2
-./configure --prefix=/usr/share/nginx --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-client-body-temp-path=/var/lib/nginx/body --http-fastcgi-temp-path=/var/lib/nginx/fastcgi --http-log-path=/var/log/nginx/access.log --http-proxy-temp-path=/var/lib/nginx/proxy --http-scgi-temp-path=/var/lib/nginx/scgi --http-uwsgi-temp-path=/var/lib/nginx/uwsgi --lock-path=/var/lock/nginx.lock --pid-path=/var/run/nginx.pid --with-pcre-jit --with-debug --with-http_addition_module --with-http_dav_module --with-http_geoip_module --with-http_gzip_static_module --with-http_image_filter_module --with-http_realip_module --with-http_stub_status_module --with-http_ssl_module --with-http_sub_module --with-http_xslt_module --with-ipv6 --with-sha1=/usr/include/openssl --with-md5=/usr/include/openssl --with-mail --with-mail_ssl_module
+./configure --prefix=/usr/local/nginx --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-client-body-temp-path=/var/lib/nginx/body --http-fastcgi-temp-path=/var/lib/nginx/fastcgi --http-log-path=/var/log/nginx/access.log --http-proxy-temp-path=/var/lib/nginx/proxy --http-scgi-temp-path=/var/lib/nginx/scgi --http-uwsgi-temp-path=/var/lib/nginx/uwsgi --lock-path=/var/lock/nginx.lock --pid-path=/var/run/nginx.pid --with-pcre-jit --with-debug --with-http_addition_module --with-http_dav_module --with-http_geoip_module --with-http_gzip_static_module --with-http_image_filter_module --with-http_realip_module --with-http_stub_status_module --with-http_ssl_module --with-http_sub_module --with-http_xslt_module --with-ipv6 --with-sha1=/usr/include/openssl --with-md5=/usr/include/openssl --with-mail --with-mail_ssl_module
 make
 make install
 
@@ -25,7 +25,7 @@ make install
 cd ~
 
 curl -L https://ghost.org/zip/ghost-latest.zip -o ghost.zip
-unzip -uo ghost.zip -d /www/ghost/
+unzip -uo ghost.zip -d /www/ems/ghost/
 cd /www/ghost/
 curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -
 apt-get install -y nodejs
