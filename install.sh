5b1b5fb9e888a52e210b6c0d5a47550d57837a6f
Mark Tomlin <Dygear@gmail.com>
2015-08-15 10:15:00 -0400

    Reconfigured Script
    It now installs ghost first, as that takes care of the update and upgrade parts of the script for us.

--- install.sh
+++ install.sh
@@ -1,41 +1,51 @@
 #!/bin/bash
 
-# On Digital Ocean, you can use this file as a cloud-init script.
+# Install Ghost Blogging Platform (And it's dependencies)
+mkdir -p /www/ems/ghost/
+curl -L https://ghost.org/zip/ghost-latest.zip -o ghost.zip
+unzip -uo ghost.zip -d /www/ems/ghost/
+cd /www/ems/ghost/
+curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -
+apt-get install -y nodejs
 
-apt-get -y update
-apt-get -y upgrade
+# Update & Upgrade Server, plus install all dependencies.
+# apt-get -y update
+# apt-get -y upgrade
 apt-get -y install build-essential libgd2-xpm-dev libgeoip-dev libpcre3-dev libssl-dev curl libcurl3 libcurl3-dev libmcrypt-dev libxslt-dev libxml2-dev sendmail
 
+# Go Home
 cd ~
 
+# Install nginx
 wget http://nginx.org/download/nginx-1.9.3.tar.gz
 tar xvfz nginx-1.9.3.tar.gz
 cd nginx-1.9.3
 ./configure --prefix=/usr/local/nginx --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-client-body-temp-path=/var/lib/nginx/body --http-fastcgi-temp-path=/var/lib/nginx/fastcgi --http-log-path=/var/log/nginx/access.log --http-proxy-temp-path=/var/lib/nginx/proxy --http-scgi-temp-path=/var/lib/nginx/scgi --http-uwsgi-temp-path=/var/lib/nginx/uwsgi --lock-path=/var/lock/nginx.lock --pid-path=/var/run/nginx.pid --with-pcre-jit --with-debug --with-http_addition_module --with-http_dav_module --with-http_geoip_module --with-http_gzip_static_module --with-http_image_filter_module --with-http_realip_module --with-http_stub_status_module --with-http_ssl_module --with-http_sub_module --with-http_xslt_module --with-ipv6 --with-sha1=/usr/include/openssl --with-md5=/usr/include/openssl --with-mail --with-mail_ssl_module
 make
 make install
-mkdir /var/lib/nginx
-mkdir /var/lib/nginx/body
+mkdir -p /var/lib/nginx
 
+# Go Home
 cd ~
 
+# Install PHP
 wget https://downloads.php.net/~ab/php-7.0.0beta3.tar.gz
 tar xvfz php-7.0.0beta3.tar.gz
 cd php-7.0.0beta3
 ./configure --enable-fpm --with-mcrypt --with-zlib --enable-mbstring --with-openssl --with-gd --with-jpeg-dir=/usr/lib --with-png-dir=/usr/lib --enable-gd-native-ttf --with-curl --enable-ftp --enable-calendar --enable-pcntl --enable-soap
 make
-#make test
+# make test
 make install
+
+# Install init.d script
 cp ./sapi/fpm/init.d.php-fpm /etc/init.d/php-fpm
 chmod u+x /etc/init.d/php-fpm
 update-rc.d php-fpm defaults
 
-cd ~
-
-curl -L https://ghost.org/zip/ghost-latest.zip -o ghost.zip
-unzip -uo ghost.zip -d /www/ems/ghost/
-cd /www/ems/ghost/
-curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -
-apt-get install -y nodejs
+# Install Config Files
+cp ./sapi/fpm/php-fpm.conf /usr/local/etc/php-fpm.conf
+mkdir -p /usr/local/NONE/etc/php-fpm.d/
+cp ./sapi/fpm/www.conf /usr/local/NONE/etc/php-fpm.d/www.conf
+nano /usr/local/NONE/etc/php-fpm.d/www.conf
 
 cd ~
