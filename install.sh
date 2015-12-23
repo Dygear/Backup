409bf62a6c9783ca1f001140e7db6630027903fb
Mark Tomlin <Dygear@gmail.com>
2015-12-23 05:58:25 -0500

    Updated nginx 1.9.9

--- install.sh
+++ install.sh
@@ -17,9 +17,9 @@ apt-get -y install build-essential libgd2-xpm-dev libgeoip-dev libpcre3-dev libs
 cd ~
 
 # Install nginx
-wget http://nginx.org/download/nginx-1.9.7.tar.gz
-tar xvfz nginx-1.9.7.tar.gz
-cd nginx-1.9.7
+wget http://nginx.org/download/nginx-1.9.9.tar.gz
+tar xvfz nginx-1.9.9.tar.gz
+cd nginx-1.9.9
 ./configure --prefix=/usr/local/nginx --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-client-body-temp-path=/var/lib/nginx/body --http-fastcgi-temp-path=/var/lib/nginx/fastcgi --http-log-path=/var/log/nginx/access.log --http-proxy-temp-path=/var/lib/nginx/proxy --http-scgi-temp-path=/var/lib/nginx/scgi --http-uwsgi-temp-path=/var/lib/nginx/uwsgi --lock-path=/var/lock/nginx.lock --pid-path=/var/run/nginx.pid --with-pcre-jit --with-debug --with-http_addition_module --with-http_dav_module --with-http_geoip_module --with-http_gzip_static_module --with-http_image_filter_module --with-http_realip_module --with-http_stub_status_module --with-http_ssl_module --with-http_sub_module --with-http_xslt_module --with-ipv6 --with-sha1=/usr/include/openssl --with-md5=/usr/include/openssl --with-mail --with-mail_ssl_module --with-http_v2_module
 make
 make install
