058b25389d1dbe9cf2f1fcfbdd6c997113540046
Mark Tomlin <Dygear@gmail.com>
2021-02-16 20:06:38 -0500

    Removed nginx updated php.

--- install.sh
+++ install.sh
@@ -8,24 +8,13 @@ apt -y install openssl libssl-dev libxml2-dev libonig-dev sqlite3 libsqlite3-dev
 # Go Home
 cd ~
 
-# Install nginx
-wget https://nginx.org/download/nginx-1.19.4.tar.gz
-tar xf nginx-1.19.4.tar.gz
-cd nginx-1.19.4
-./configure --enable-fpm --with-zlib --enable-mbstring --with-openssl --enable-calendar --enable-pcntl --enable-soap --enable-debug --enable-opcache
-make -j `nproc`
-make install
-mkdir -p /var/lib/nginx
-
-# Install init.d script
-
 # Go Home
 cd ~
 
 # Install PHP
-wget https://php.net/distributions/php-8.0.0.tar.xz
-tar xf php-8.0.0.tar.xz
-cd php-8.0.0
+wget https://php.net/distributions/php-8.0.2.tar.xz
+tar xf php-8.0.2.tar.xz
+cd php-8.0.2
 ./configure --enable-fpm --enable-pcntl --enable-calendar --enable-mbstring --with-zlib --with-openssl --with-libxml --enable-soap
 make -j`nproc`
 #make test
