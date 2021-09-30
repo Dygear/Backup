b4c77fd99fb30117dc565638e378aa4efbb51877
Mark Tomlin <Dygear@gmail.com>
2021-09-30 00:18:36 -0400

    PHP 8.1.0RC3

--- install.sh
+++ install.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
 # Update & Upgrade Server, plus install all dependencies.
-# apt -y update
-# apt -y upgrade
+apt -y update
+apt -y upgrade
 apt -y install pkg-config openssl libssl-dev libxml2-dev libonig-dev sqlite3 libsqlite3-dev libcurl4-openssl-dev zlib1g-dev libpng-dev
 
 # Go Home
 cd ~
 
-# Go Home
-cd ~
-
 # Install PHP
-wget https://php.net/distributions/php-8.0.8.tar.xz
-tar xf php-8.0.8.tar.xz
-cd php-8.0.3
+wget https://php.net/distributions/php-8.1.0RC3.tar.xz
+tar xf php-8.1.0RC3.tar.xz
+cd php-8.1.0RC3
 ./configure --enable-fpm --enable-pcntl --enable-calendar --enable-mbstring --with-zlib --with-openssl --with-libxml --enable-soap
 make -j`nproc`
-#make test
+make TEST_PHP_ARGS=-j`nproc` test
 make install
 
 # Install init.d script
