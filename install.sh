06f1bb46c364f68fc21f0b22719c59bfaa9123b2
Mark Tomlin <Dygear@gmail.com>
2022-12-23 18:11:54 -0500

    PHP 8.2.0

--- install.sh
+++ install.sh
@@ -12,9 +12,9 @@ apt -y install nginx build-essential pkg-config openssl libssl-dev libxml2-dev l
 cd ~
 
 # Install PHP
-wget https://www.php.net/distributions/php-8.1.8.tar.xz
-tar xf php-8.1.8.tar.xz
-cd php-8.1.8
+wget https://www.php.net/distributions/php-8.2.0.tar.xz
+tar xf php-8.2.0.tar.xz
+cd php-8.2.0
 ./configure --enable-fpm --enable-pcntl --enable-calendar --enable-mbstring --with-zlib --with-openssl --with-libxml --enable-soap
 make -j`nproc`
 make TEST_PHP_ARGS=-j`nproc` test
